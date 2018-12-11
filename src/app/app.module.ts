4ddc5e7636ca7e142e69be0e386fe60390106750
kishangabani <kishan.gabani600@gmail.com>
2018-12-11 15:01:36 -0500

    interpolation and create method

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component'; // Automatic update courses component
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component'; // Automatic update courses component
 
 @NgModule({
   declarations: [
     AppComponent,
+    CourseComponent,
     CoursesComponent
+    
   ],
   imports: [
     BrowserModule,
--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,9 +2,13 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'courses',             //basics CSS    <courses> "courses" 
-    template:'<h2>Courses</h2>'     //<div class ="courses"> ".courses" 
+    template:'<h2>{{getTitle()}}</h2>'     //<div class ="courses"> ".courses" 
                             //<div id ="courses"> "#courses" 
 })
 export class CoursesComponent{
+    title="List of courses";
 
+    getTitle(){
+        return this.title;
+    }
 }
